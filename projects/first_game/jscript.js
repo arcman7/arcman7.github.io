3550ac7fa6fc9a863a814d2eb300417232fc9fbe
Andrew Ryan Carpenter <andrewryancarpenter77@gmail.com>
2015-04-13 20:55:05 -0700

    test heal settings

--- projects/first_game/jscript.js
+++ projects/first_game/jscript.js
@@ -219,7 +219,7 @@ var turn_counter = 0;
   function checkForDead(){
     for(person in team){
       alert("maxTurn = " + maxTurn+", "+team[person].klass+" = "+team[person].health );
-      if(team[person].heath <= 0){
+      if(team[person].health <= 0){
          maxTurn -= 1;
          alert("maxTurn = " + maxTurn);
       }
